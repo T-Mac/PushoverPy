b70aafc8876bc6ec02010153ca93e3f192dd9636
Taylor McKinnon <htmlfreak117@gmail.com>
2013-02-08 01:19:09 -0600

    Initial upload and release

---
+++ Pushover.py
@@ -0,0 +1,55 @@
+﻿"""
+	Copyright 2013 Taylor McKinnon
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import httplib, urllib
+from optparse import OptionParser
+
+USER_ID="T5PbJ2nkZJ6hdx44tFwHnVsIVLBGNz"
+API_TOKEN="FrXhv6faJur2WxBeCjIqfTaEIq3uWb"
+
+def send_message(message):
+	conn = httplib.HTTPSConnection("api.pushover.net:443")
+	conn.request("POST", "/1/messages.json",
+	urllib.urlencode({
+		"token": API_TOKEN,
+		"user": USER_ID,
+		"message": message,
+	}), { "Content-type": "application/x-www-form-urlencoded" })
+	conn.getresponse()
+	
+	
+def send_silent(message):
+	conn = httplib.HTTPSConnection("api.pushover.net:443")
+	conn.request("POST", "/1/messages.json",
+	urllib.urlencode({
+		"token": API_TOKEN,
+		"user": USER_ID,
+		"message": message,
+		"sound":"none",	
+	}), { "Content-type": "application/x-www-form-urlencoded" })
+	conn.getresponse()
+	
+	
+parser = OptionParser()
+parser.add_option("-m", "--message", dest="message",
+				help="set message for notification", metavar="MESSAGE")
+parser.add_option("-s", "--silent",
+				action='store_true', dest="silent", default = False,
+				help="silent notification")
+(options, args) = parser.parse_args()
+if options.silent:
+	send_silent(options.message)
+else:
+	send_message(options.message)
+				  
